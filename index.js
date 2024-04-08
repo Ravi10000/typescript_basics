5df14f0e25ae6b5e2dc0a8f664c256d9f26ce27a
@raviSince2k <ravisince2k@gmail.com>
2024-04-08 18:05:11 +0530

    chapter 2: everyday types till object types

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-console.log("Hello TypeScript");
 function greet(name, date) {
     console.log(`Hello, ${name}`);
     console.log(`Today is ${date.toDateString()}`);
 }
+function add(numbers) {
+    return new Promise((resolve, reject) => {
+        resolve(numbers.reduce((n, acc) => (acc += n), 0));
+    });
+}
+function printCoordinates(pt) {
+    console.log(`[${pt.x}][${pt.y}]`);
+}
+function printGuestDetails(guest) {
+    console.log(`guest name ${guest === null || guest === void 0 ? void 0 : guest.name}`);
+    if (guest.id !== undefined)
+        console.log(`guest id ${guest.id}`);
+}
 greet("Ravi", new Date());
+add([10, 20, 10]).then((number) => {
+    console.log(number);
+});
+printCoordinates({ x: 10, y: 10 });
+printGuestDetails({ name: "Ravi" });
--- index.ts
+++ index.ts
@@ -1,8 +1,26 @@
-console.log("Hello TypeScript");
-
 function greet(name: string, date: Date) {
   console.log(`Hello, ${name}`);
   console.log(`Today is ${date.toDateString()}`);
 }
 
+function add(numbers: number[]): Promise<number> {
+  return new Promise((resolve, reject) => {
+    resolve(numbers.reduce((n, acc) => (acc += n), 0));
+  });
+}
+
+function printCoordinates(pt: { x: number; y: number }) {
+  console.log(`[${pt.x}][${pt.y}]`);
+}
+
+function printGuestDetails(guest: { name: string; id?: number }) {
+  console.log(`guest name ${guest?.name}`);
+  if (guest.id !== undefined) console.log(`guest id ${guest.id}`);
+}
+
 greet("Ravi", new Date());
+add([10, 20, 10]).then((number) => {
+  console.log(number);
+});
+printCoordinates({ x: 10, y: 10 });
+printGuestDetails({ name: "Ravi" });
